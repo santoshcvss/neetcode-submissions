6c50bc6c60b9844e895a9b0f36a92ca85d390550
santoshcvss <63705290+santoshcvss@users.noreply.github.com>
2026-09-12 00:58:57 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+        unordered_map<int,vector<int>> m;
+        vector<int> in(numCourses,0);
+        for(int i=0;i<prereq.size();i++){
+            m[prereq[i][0]].push_back(prereq[i][1]);
+            in[prereq[i][1]]++;
+        }
+        int count=0;
+        queue<int> q;
+        for(int i=0;i<in.size();i++){
+            if(in[i]==0){
+                q.push(i); 
+            }
+        }
+        while(!q.empty()){
+            count++;
+            int f=q.front();q.pop();
+            for(int i=0;i<m[f].size();i++){
+                in[m[f][i]]--;
+                if(in[m[f][i]]==0) q.push(m[f][i]);
+            }
+        }
+        if(count==numCourses) return true;
+        return false;
+    }
+};
