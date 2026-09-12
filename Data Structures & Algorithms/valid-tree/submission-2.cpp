1012758e9380ee5a3e2860687519d24ebeeddbc0
santoshcvss <63705290+santoshcvss@users.noreply.github.com>
2026-09-13 00:58:34 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size()!=n-1) return false;
+        unordered_map<int, vector<int>> m;
+        vector<int> vis(n,0);
+        for(int i=0;i<edges.size();i++){
+            m[edges[i][1]].push_back(edges[i][0]);
+            m[edges[i][0]].push_back(edges[i][1]);
+        }
+        queue<int> q;
+        q.push(0);
+        int count=1;
+        while(!q.empty()){
+            int f=q.front(); q.pop();
+            vis[f]=1;
+            for(int i=0;i<m[f].size();i++){
+                if(vis[m[f][i]]!=1){
+                    q.push(m[f][i]);
+                    count++;
+                }
+            }
+        }
+        if(count==n) return true;
+        return false;
+    }
+};
